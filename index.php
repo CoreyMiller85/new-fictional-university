2ac775fe08211a3f2494fecd08af350452a5c6e8
Corey Miller <coreymiller454@gmail.com>
2022-05-01 20:38:47 -0400

    initial commit

---
+++ index.php
@@ -0,0 +1,10 @@
+<h1>This is a test</h1>
+
+<?php 
+    while(have_posts()) {
+        the_post(); ?>
+        <h2><a href="<?php the_permalink(); ?>"> <?php the_title(); ?></a></h2>
+        <?php the_content(); ?>
+        <hr>
+  <?php  }
+?>
